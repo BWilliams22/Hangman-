7e290d8045d4604aa4ca4ce86dc9123aa8a29fc2
BWilliams22 <1williamreb2@hdsb.ca>
2020-07-24 15:19:42 +0000

    Added guesses

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 print("Hello, and welcome to Hangman! I'm glad you can play...All you have to do is guess letters! If the letter is in the word I have in mind...GREAT! However, if it isn't, then you will get part of the body of my man. If you build the whole man before you guess my word...you lose! However if you guess my word...you win! Good luck and feel free to leave at any time!")
+print("WARNING!!! You only have six guesses, the head, body, two arms and two legs. If you guess six guesses wrong, you lose!")
+print(" O ")
+print("/|\ ")
+print("/ \ ")
 
 import random 
 
@@ -9,7 +13,6 @@ with open("words.txt", "r") as f:
     words.append(line.strip()) 
 
 secret_word = random.choice(words)
-print(secret_word)
 
 correct_guessed = ""
 incorrect_guessed = ""
@@ -27,11 +30,35 @@ while not gameover and set(correct_guessed) != set(secret_word):
   if guess in secret_word:
     correct_guessed += guess
     print("Correct!")
+    for letter in secret_word:
+      if letter in correct_guessed: 
+       print(letter, end = " ") 
+      else:
+       print("_", end = " ")
   else: 
     incorrect_guessed += guess
     print("Sorry, that's not in the word!")
     print("Guessed letters that are incorrect: "+" , ".join(sorted(incorrect_guessed))) 
     lives -= 1 
+    if lives == 5:
+      print(" O ")
+    elif lives == 4: 
+      print(" O ")
+      print(" | ")
+    elif lives == 3: 
+      print(" O ")
+      print("/|")
+    elif lives == 2: 
+      print(" O ")
+      print("/|\ ")
+    elif lives == 1: 
+      print(" O ")
+      print("/|\ ")
+      print("/ ")
+    elif lives == 0: 
+      print(" O ")
+      print("/|\ ")
+      print("/ \ ")
     if lives > 0: 
      gameover = False  
     else:  
